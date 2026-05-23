c03349526a261b44ae7e7b71852ffadf579c8dec
Rafael Coelho <coelho38r@proton.me>
2026-05-23 12:17:22 -0300

    s

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-IMAGE="${RINHA_IMAGE:-ghcr.io/rafaelcoelhox/eu-sou-o-ze-pamonha:pmh}"
+IMAGE="${RINHA_IMAGE:-ghcr.io/rafaelcoelhox/eu-sou-o-ze-pamonha:pmhh}"
 
 docker buildx build \
   --platform linux/amd64 \
